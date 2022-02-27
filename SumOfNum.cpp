8ecf75e462972f3eac684d342634950197a81ced
Manish Kumar <manish.2101290@sanskriti.ac.in>
2022-02-27 23:42:46 +0530

    Sum Of Two Numbers

---
+++ SumOfNum.cpp
@@ -0,0 +1,25 @@
+//  Program to print the sum of two numbers using Variables
+
+# include<iostream>
+using namespace std;
+int main(){
+    int Num1, Num2, Sum;
+
+    Num1 = 25;
+    Num2 = 69;
+    Sum = Num1+Num2; // Here Num1 and Num2 are two operands and + is an arithmetic Operator
+    cout<<"Sum of the 25 and 69 is = "<<Sum<<endl;
+
+// Sum of Two Numbers using user input and without third variable 
+
+    int Number1 , Number2, Add;
+
+    cout<<"Enter Number1 : "<<endl;
+    cin>>Number1;
+    cout<<"Enter Number2 : \n";
+    cin>>Number2;
+    cout<<"Sum of Numbers is = "<<Number1+Number2;
+
+    return 0;
+} 
+
