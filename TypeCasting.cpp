c14b15c5871df52ed87709d7713ee8d468d0efc6
Manish Kumar <manish.2101290@sanskriti.ac.in>
2022-02-27 19:08:42 +0530

    Type Casting in C++

---
+++ TypeCasting.cpp
@@ -0,0 +1,19 @@
+// Type Casting is a process of conversion of one data type to another.
+
+# include <iostream>
+using namespace std;
+int main(){
+int Character =  'A' ; //  It will print the Decimal Ascii Value of character A .
+cout<<Character << endl;
+
+char Integer = 64; // It will print the character Ascii value of integer 54 .
+cout<<Integer << endl;
+
+int Float = 14.56; // It  will print only integer part of the Float 14.56 .
+cout<<Float <<endl;
+
+float Num = 84;
+cout<<Num;
+
+return 0; 
+}
