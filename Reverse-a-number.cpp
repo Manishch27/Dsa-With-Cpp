72811e3ca61094c7a9f63e3abd4f4c1caa913ae4
Manish Kumar <manish.2101290@sanskriti.ac.in>
2022-05-11 22:33:41 +0530

    Program to reverse a number

---
+++ Reverse-a-number.cpp
@@ -0,0 +1,21 @@
+//  Program to reverse a number
+
+# include <iostream>
+using namespace std;
+void rev(int num){
+    int reverse = 0;
+    cout<<"Enter a number : "<<endl;
+    cin>>num;
+    while(num!=0){
+        int rem = num%10;
+        reverse *=10+rem;
+        num/=10;
+        cout<<rem;
+    }
+}
+
+int main(){
+    int num;
+    rev(num);
+    return 0;
+    }
