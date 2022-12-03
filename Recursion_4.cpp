155db74f99037a0aae47a8abacd8161c13d05b07
Manish Kumar <Manishchaudhary2705@gmail.com>
2022-12-03 22:10:16 +0530

    Print Nth fibonacci number

---
+++ Recursion_4.cpp
@@ -0,0 +1,25 @@
+// Program to print the Nth fibonacci number
+
+# include<iostream>
+using namespace std;
+
+int fib(int *num){
+    if(*num==0){
+        return 0;
+    }
+
+    if(*num==1){
+        return 1;
+    }
+    return fib(*n-1)+fib(*n-2);
+}
+
+int main(){
+    int num;
+    cout<<"Enter the position "<<endl;
+    cin>>num;
+
+    cout<<fib(&num);
+
+    return 0;
+}
