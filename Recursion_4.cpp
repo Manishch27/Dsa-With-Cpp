f51fd9a04d327801b71c60e615adaafe53d04f0f
Manish Kumar <manishchaudhary2705@gmail.com>
2022-12-04 17:03:07 +0530

    Update Recursion_4.cpp

--- Recursion_4.cpp
+++ Recursion_4.cpp
@@ -3,15 +3,15 @@
 # include<iostream>
 using namespace std;
 
-int fib(int *num){
-    if(*num==0){
+int fib(int num){
+    if(num==0){
         return 0;
     }
 
-    if(*num==1){
+    if(num==1){
         return 1;
     }
-    return fib(*n-1)+fib(*n-2);
+    return fib(num-1)+fib(num-2);
 }
 
 int main(){
